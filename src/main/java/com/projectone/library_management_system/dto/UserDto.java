158226ff37b56940c0ede2c58ce983470636d3be
Christian <Christianmnaira@gmail.com>
2025-11-20 00:49:18 +0800

    feat: added and designed user data transfer object

---
+++ src/main/java/com/projectone/library_management_system/dto/UserDto.java
@@ -0,0 +1,41 @@
+package com.projectone.library_management_system.dto;
+
+public class UserDto {
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasssword() {
+        return passsword;
+    }
+
+    public void setPasssword(String passsword) {
+        this.passsword = passsword;
+    }
+
+    private String username;
+    private String email;
+    private String passsword;
+
+    public UserDto() {}
+
+    public UserDto(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.passsword = password;
+    }
+
+}
