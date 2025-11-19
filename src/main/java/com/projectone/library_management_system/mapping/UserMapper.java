b4b1bff160b31d8096c06391cbe150e1926217a2
Christian <Christianmnaira@gmail.com>
2025-11-20 01:10:03 +0800

    fix: design the mapper for entity and data transfer object

---
+++ src/main/java/com/projectone/library_management_system/mapping/UserMapper.java
@@ -0,0 +1,27 @@
+package com.projectone.library_management_system.mapping;
+
+import com.projectone.library_management_system.dto.UserDto;
+import com.projectone.library_management_system.entity.Users;
+import org.apache.catalina.User;
+
+public class UserMapper {
+
+    // UserDTO to User Entity
+    public static Users toEntity(UserDto dto) {
+        if (dto == null) return null;
+        return new Users(dto.getUsername(), dto.getEmail(), dto.getPasssword());
+    }
+
+    // User Entity to UserDTO
+    public static UserDto toDto(Users users) {
+        if (users == null) return null;
+
+        UserDto dto = new UserDto();
+        dto.setUsername(users.getUsername());
+        dto.setEmail(users.getEmail());
+
+        return dto;
+
+    }
+
+}
