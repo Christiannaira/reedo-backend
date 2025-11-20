1d0f5c21df2e2ae2afaa46e800b5dabf9516afe4
Christian <Christianmnaira@gmail.com>
2025-11-20 10:46:16 +0800

    fix: added a new DTO class for client response

---
+++ src/main/java/com/projectone/library_management_system/dto/UserResponseDto.java
@@ -0,0 +1,83 @@
+package com.projectone.library_management_system.dto;
+
+import java.time.LocalDateTime;
+
+public class UserResponseDto {
+
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+    private LocalDateTime dateCreated;
+
+    public UserResponseDto() {}
+
+    public UserResponseDto(String username, String email, String firstName, String lastName, String address, String phoneNumber, LocalDateTime dateCreated) {
+        this.username = username;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.dateCreated = dateCreated;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+}
--- src/main/java/com/projectone/library_management_system/mapping/UserMapper.java
+++ src/main/java/com/projectone/library_management_system/mapping/UserMapper.java
@@ -1,15 +1,16 @@
 package com.projectone.library_management_system.mapping;
 
 import com.projectone.library_management_system.dto.UserDto;
+import com.projectone.library_management_system.dto.UserRequestDto;
 import com.projectone.library_management_system.entity.Users;
 import org.apache.catalina.User;
 
 public class UserMapper {
 
     // UserDTO to User Entity
-    public static Users toEntity(UserDto dto) {
+    public static Users toEntity(UserRequestDto dto) {
         if (dto == null) return null;
-        return new Users(dto.getUsername(), dto.getEmail(), dto.getPasssword());
+        return new Users(dto.getUsername(), dto.getEmail(), dto.getPassword());
     }
 
     // User Entity to UserDTO
